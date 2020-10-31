a86feb85e4c4fd7a95d28645919121178015e207
adrianofelix019 <46236684+adrianofelix019@users.noreply.github.com>
2020-10-30 21:58:44 -0300

    Initial commit.

---
+++ src/script.js
@@ -0,0 +1,33 @@
+function calculate() {
+    const result = document.querySelector('#result-container')
+    const binNumber = String(document.querySelector('#bin').value)
+
+    if(validateInput(binNumber)) {
+        result.innerHTML = `<p>${binaryToDecimal(reverse(binNumber))}</p>`
+    }
+}
+
+function binaryToDecimal(binaryString) {
+    let decimal = '0'
+
+    for(let i = 0;i < binaryString.length;i++) {
+        decimal = String(Number(decimal) + Number(binaryString[i] * 2**i))
+    }
+
+    return decimal
+}
+
+function validateInput(input) {
+    for(let i = 0;i < input.length;i++) {
+        if(input[i] !== '0' && input[i] !== '1') {
+            alert('Please enter binary digits only')
+            return false
+        }
+    }
+
+    return true
+}
+
+function reverse(num) {
+    return num.split('').reverse().join('')
+}
