72b9ed7d2f2f8315a4eb0c83928b4d502636164d
adrianofelix019 <46236684+adrianofelix019@users.noreply.github.com>
2020-11-24 01:29:49 -0300

    indentation correction

--- src/script.js
+++ src/script.js
@@ -1,33 +1,33 @@
 function calculate() {
-    const result = document.querySelector('#result-container')
-    const binNumber = String(document.querySelector('#bin').value)
+  const result = document.querySelector('#result-container')
+  const binNumber = String(document.querySelector('#bin').value)
 
-    if(validateInput(binNumber)) {
-        result.innerHTML = `<p>${binaryToDecimal(reverse(binNumber))}</p>`
-    }
+  if (validateInput(binNumber)) {
+    result.innerHTML = `<p>${binaryToDecimal(reverse(binNumber))}</p>`
+  }
 }
 
 function binaryToDecimal(binaryString) {
-    let decimal = '0'
+  let decimal = '0'
 
-    for(let i = 0;i < binaryString.length;i++) {
-        decimal = String(Number(decimal) + Number(binaryString[i] * 2**i))
-    }
+  for (let i = 0; i < binaryString.length; i++) {
+    decimal = String(Number(decimal) + Number(binaryString[i] * 2 ** i))
+  }
 
-    return decimal
+  return decimal
 }
 
 function validateInput(input) {
-    for(let i = 0;i < input.length;i++) {
-        if(input[i] !== '0' && input[i] !== '1') {
-            alert('Please enter binary digits only')
-            return false
-        }
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== '0' && input[i] !== '1') {
+      alert('Please enter binary digits only')
+      return false
     }
+  }
 
-    return true
+  return true
 }
 
 function reverse(num) {
-    return num.split('').reverse().join('')
+  return num.split('').reverse().join('')
 }
